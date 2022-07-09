df29438016e8f83f12ff9f532434d2f9ea71c0e3
Gabriel Claudino Marinho <gabriel.cmunivos@protonmail.com>
2022-07-09 12:37:12 -0300

    fix: loading error

--- src/app/presentation/views/home/companies/companies.component.ts
+++ src/app/presentation/views/home/companies/companies.component.ts
@@ -15,6 +15,9 @@ export class CompaniesComponent implements OnInit {
 
   ngOnInit() {
     this.getListCompanies();
+    setTimeout(() => {
+        this.loading = false
+    }, 1000);
   }
 
   getListCompanies() {
@@ -25,7 +28,7 @@ export class CompaniesComponent implements OnInit {
         this.hasCompany = true
       }
     });
-    this.loading = false;
+
   }
 
   setCompanies(companies: ICompanies[]) {
